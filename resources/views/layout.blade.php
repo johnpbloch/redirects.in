c74b579dc306b232237a1d6cf97c2325b18308a0
John P Bloch <johnpbloch@gmail.com>
2015-06-17 22:21:41 -0500

    Reformat layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title')Redirects.in</title>
-    </head>
-    <body>
-        <div id="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-                <div class="quote">{{ Inspiring::quote() }}</div>
-            </div>
-        </div>
-    </body>
+<head>
+    <title>@yield('title')Redirects.in</title>
+</head>
+<body>
+<div id="container">
+    <div class="content">
+        <div class="title">Laravel 5</div>
+        <div class="quote">{{ Inspiring::quote() }}</div>
+    </div>
+</div>
+</body>
 </html>
