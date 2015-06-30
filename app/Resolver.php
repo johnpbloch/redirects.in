162be2d2cb30fa1553daf62986703d156381df3b
John P Bloch <johnpbloch@gmail.com>
2015-06-30 18:26:29 -0500

    Add a method to resolve a url's redirections

--- app/Resolver.php
+++ app/Resolver.php
@@ -5,4 +5,13 @@ namespace App;
 class Resolver
 {
 
+    /**
+     * Resolve the redirections in a given URL
+     *
+     * @param string $url
+     */
+    public function resolve( $url )
+    {
+    }
+
 }
