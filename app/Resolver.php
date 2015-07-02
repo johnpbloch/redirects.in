df714bf266899d8b6eac87c7d0eac60e89ba89b7
John P Bloch <johnpbloch@gmail.com>
2015-07-01 22:26:17 -0500

    Fill in resolver functionality

--- app/Resolver.php
+++ app/Resolver.php
@@ -2,6 +2,11 @@
 
 namespace App;
 
+use App;
+use Guzzle;
+use Psr\Http\Message\ResponseInterface;
+use League\Url\Url;
+
 class Resolver
 {
 
@@ -22,7 +27,69 @@ class Resolver
      */
     public function resolve( $url )
     {
+        $urlObject     = $this->getUrlObject( $url );
+        $this->steps[] = (string) $urlObject;
+        while ($urlObject = $this->getRedirectLocation( $urlObject )) {
+            $urlString = (string) $urlObject;
+            if (false !== $this->steps->find( $urlString )) {
+                $this->steps[] = 'Infinite redirect';
+                break;
+            }
+            $this->steps[] = $urlString;
+        }
+
         return $this->steps->getAllSteps();
+    }
+
+    /**
+     * @param $url
+     *
+     * @return \League\Url\Url
+     */
+    protected function getUrlObject( $url )
+    {
+        /** @var \League\Url\Url $urlObject */
+        $urlObject = App::make( 'League\Url\Url', [ $url ] );
+        if ( ! $urlObject->getScheme()->get()) {
+            $urlObject->setScheme( 'http' );
+        }
+
+        return $urlObject;
+    }
+
+    /**
+     * Get the location of a redirect or false if none
+     *
+     * @param Url $url
+     *
+     * @return Url|false
+     */
+    protected function getRedirectLocation( Url $url )
+    {
+        /** @var ResponseInterface $response */
+        $response = Guzzle::get( (string) $url, [ 'allow_redirects' => false ] );
+        if (
+            ! env( 'ALLOW_INSANE_STATUS_CODES', false ) &&
+            ( $response->getStatusCode() > 399 || $response->getStatusCode() < 300 )
+        ) {
+            return false;
+        }
+        $rawLocation = $response->getHeader( 'location' );
+        if ( ! $rawLocation) {
+            return false;
+        }
+        $rawLocation = $rawLocation[0];
+        $newUrl      = $this->getUrlObject( $rawLocation );
+        if ( ! $newUrl->getHost()->get()) {
+            $newUrl->setHost( $url->getHost()->get() );
+            if ('/' !== $rawLocation[0]) {
+                $newUrl->setPath(
+                    rtrim( $url->getPath()->get(), '/' ) . '/' . ltrim( $newUrl->getPath()->get(), '/' )
+                );
+            }
+        }
+
+        return $newUrl;
     }
 
 }
