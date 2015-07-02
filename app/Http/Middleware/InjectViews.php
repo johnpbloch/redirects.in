afdda7c3a72d45e79f3a8cc37dee0fdf11a2b4e8
John P Bloch <johnpbloch@gmail.com>
2015-07-01 23:08:43 -0500

    Add a format negotiator to maybe return html

--- app/Http/Middleware/InjectViews.php
+++ app/Http/Middleware/InjectViews.php
@@ -3,9 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Negotiation\FormatNegotiator;
 
 class InjectViews
 {
+
+    protected $negotiator;
+
+    function __construct( FormatNegotiator $negotiator )
+    {
+        $this->negotiator = $negotiator;
+    }
+
 
     /**
      * Handle an incoming request.
@@ -18,6 +27,13 @@ class InjectViews
     public function handle( $request, Closure $next )
     {
         $data = $next( $request );
+
+        $acceptHeader = $request->header( 'Accept' );
+        $priorities   = [ 'html', 'json', '*/*' ];
+        $format       = $this->negotiator->getBestFormat( $acceptHeader, $priorities );
+        if ('json' !== $format) {
+            return view( 'follow', [ 'redirects' => $data ] );
+        }
 
         return $data;
     }
