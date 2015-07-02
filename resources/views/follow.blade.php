7370054bdf9c34c64135605aa077cc5908ee4074
John P Bloch <johnpbloch@gmail.com>
2015-07-02 16:08:17 -0500

    Add markup to render the steps in the redirect

--- resources/views/follow.blade.php
+++ resources/views/follow.blade.php
@@ -3,5 +3,24 @@
 @section('title', isset($final) ? "$final | " : '')
 
 @section('content')
+    <div id="steps_container">
+        @unless($start === $end)
+        <div id="first_step">
+            {{ $start }}
+        </div>
+        @endunless
+        <?php $previous = $start; ?>
+        <ol>
+            @forelse($steps as $step)
+                <li><span>{{ $previous }}</span><span>{{ $step }}</span></li>
+                <?php $previous = $step; ?>
+            @empty
+                <li>No redirects</li>
+            @endforelse
+        </ol>
+        <div id="final_step">
+            {{ $end }}
+        </div>
+    </div>
     @parent
 @endsection
