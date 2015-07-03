dcce1644c5d153e4d317432c8d1741b3fe2220e0
John P Bloch <johnpbloch@gmail.com>
2015-07-03 12:58:29 -0500

    Redirect with a message if the host is blacklisted

--- app/Http/Middleware/CheckUrlBlacklist.php
+++ app/Http/Middleware/CheckUrlBlacklist.php
@@ -6,6 +6,9 @@ use Closure;
 
 class CheckUrlBlacklist
 {
+
+    private $domain_blacklist = [ ];
+
     /**
      * Handle an incoming request.
      *
@@ -16,6 +19,19 @@ class CheckUrlBlacklist
      */
     public function handle( $request, Closure $next )
     {
+        $urlString = ltrim( $request->getRequestUri(), '/' );
+        /** @var \League\Url\Url $urlObject */
+        $urlObject = app( 'League\Url\Url', [ $urlString ] );
+        if (in_array( $urlObject->getHost()->get(), $this->getBlacklist() )) {
+            return redirect( url() )->withErrors( 'That host is blacklisted!' );
+        }
+
         return $next( $request );
     }
+
+    private function getBlacklist()
+    {
+        return $this->domain_blacklist;
+    }
+
 }
