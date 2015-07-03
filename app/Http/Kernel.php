0ef770287547e57598ac1f8205fc104f804350e1
John P Bloch <johnpbloch@gmail.com>
2015-07-03 12:25:10 -0500

    Add a middleware for blacklisted request URLs

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,5 +30,6 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => Middleware\RedirectIfAuthenticated::class,
         'inject' => Middleware\InjectViews::class,
+        'blacklist' => Middleware\CheckUrlBlacklist::class
     ];
 }
